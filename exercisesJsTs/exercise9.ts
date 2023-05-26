f638c8517e09073e222e2d88914f5713641db228
WasylGutnyk <wasyl.gutnyk@reckitt.com>
2023-05-26 13:35:13 +0200

    creating object property

---
+++ exercisesJsTs/exercise9.ts
@@ -0,0 +1,17 @@
+/*
+creating obj prop with variable
+*/
+
+const testVariable = 'value';
+
+const obj = {
+  nam: 'Vasyl',
+  testVariable
+}
+
+console.log('obj', obj);
+
+// {nam: 'Vasyl', testVariable: 'value'}
+/*
+  same can be done via obj['test'] = 'smth'
+*/
